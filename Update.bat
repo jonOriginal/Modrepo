e2c2c21ff9d95c78724961576b7ea3e65f0f6049
jonOriginal <79869389+jonOriginal@users.noreply.github.com>
2021-03-02 09:04:07 +1300

    V0.0.3

---
+++ Update.bat
@@ -0,0 +1,8 @@
+cd %temp%
+rd modsrepo
+mkdir modsrepo
+cd modsrepo
+powershell.exe Invoke-RestMethod -Uri https://github.com/jonOriginal/Modrepo/archive/main.zip -OutFile main.zip
+tar -xf main.zip
+cd Modrepo-main
+xcopy "%temp%/modsrepo/Modrepo-main/*.*" "%appdata%/.minecraft/mods" /Y /f /Exclude:tlauncher.exe+forge.jar
