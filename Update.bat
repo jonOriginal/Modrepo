5f848a5f4adfd021542261343b92e819458a3bf1
jonOriginal <79869389+jonOriginal@users.noreply.github.com>
2021-03-15 07:16:00 +1300

    Delete Update.bat

--- Update.bat
+++
@@ -1,8 +0,0 @@
-cd %temp%
-rd modsrepo
-mkdir modsrepo
-cd modsrepo
-powershell.exe Invoke-RestMethod -Uri https://github.com/jonOriginal/Modrepo/archive/main.zip -OutFile main.zip
-tar -xf main.zip
-cd Modrepo-main
-xcopy /EXCLUDE:exclude.txt "%temp%/modsrepo/Modrepo-main/*.*" "%appdata%/.minecraft/mods" /Y /f 
