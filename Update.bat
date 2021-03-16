f8e1678af487de2436c010e9d090cae3ff8e538d
jonOriginal <79869389+jonOriginal@users.noreply.github.com>
2021-03-16 16:35:19 +1300

    Add files via upload

--- Update.bat
+++ Update.bat
@@ -1,8 +1,10 @@
+cd %appdata%/.minecraft/mods/
+for %%i in (*.*) do if not "%%i"=="Update.bat" if not "%%i"=="Update Mods.lnk" del /q "%%i"
 cd %temp%
 rd modsrepo
 mkdir modsrepo
 cd modsrepo
-powershell.exe Invoke-RestMethod -Uri https://github.com/jonOriginal/Modrepo/archive/main.zip -OutFile main.zip
+powershell.exe $global:progressPreference = 'silentlyContinue'; Invoke-RestMethod -Uri https://github.com/jonOriginal/Modrepo/archive/main.zip -OutFile main.zip
 tar -xf main.zip
 cd Modrepo-main
-xcopy /EXCLUDE:exclude.txt "%temp%/modsrepo/Modrepo-main/*.*" "%appdata%/.minecraft/mods" /Y /f 
+xcopy "%temp%/modsrepo/Modrepo-main/*.*" "%appdata%/.minecraft/mods" /Y /f 
